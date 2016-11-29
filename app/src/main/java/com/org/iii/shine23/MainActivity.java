eb5723d01d25a8eaa3005b156250433f287f6d33
user <a>
2016-11-29 15:36:30 +0800

    v3 相機

--- app/src/main/java/com/org/iii/shine23/MainActivity.java
+++ app/src/main/java/com/org/iii/shine23/MainActivity.java
@@ -3,15 +3,17 @@ package com.org.iii.shine23;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,8 +43,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1 && resultCode == RESULT_OK){
-
+            take1(data);
         }
+    }
+
+    //縮圖 資料放在INTENT裡面
+    private void take1(Intent it){
+        Bitmap bmp = (Bitmap)it.getExtras().get("data");
+        img.setImageBitmap(bmp);
     }
 
 
